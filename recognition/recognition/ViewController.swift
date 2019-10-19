5f04b6207b4e5f726c0f4a826a3946cd6bf6f0db
kemalsen <kemalsencs@yahoo.com>
2019-10-19 15:49:38 +0300

    Add files via upload

---
+++ recognition/recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recognition
+//
+//  Created by Kemal Şen on 19.10.2019.
+//  Copyright © 2019 Kemal Şen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
